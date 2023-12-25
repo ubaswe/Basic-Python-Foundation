c11be028ca63938fa46460c17f064f186a23cbf2
Ubaswe <127177765+ubaswe@users.noreply.github.com>
2023-12-26 02:31:50 +0600

    Update User input.py

--- User input.py
+++ User input.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
 
 #string by user input
 x=input("Enter an word having 5 letter :")
@@ -10,9 +5,6 @@ print(x)
 print(x[1])
 print(x[1-3])
 print(x[-3-1])
-
-
-# In[1]:
 
 
 #Arthmatic Operation by user input
